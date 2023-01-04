9396e70bcdc925b801ed07d9e45a5d6b98ecec86
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-01-04 13:34:24 +0530

    Added : Calculate the Length of the Line

---
+++ src/main/java/com/BridgeLabz/LineComparison/LineComaprison.java
@@ -0,0 +1,25 @@
+package com.BridgeLabz.LineComparison;
+
+import java.util.Scanner;
+
+public class LineComaprison {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter the Number for X1 : ");
+		int x1 = scan.nextInt();
+		System.out.print("Enter the Number for y1 : ");
+		int y1 = scan.nextInt();
+		System.out.print("Enter the Number for X2 : ");
+		int x2 = scan.nextInt();
+		System.out.print("Enter the Number for y2 : ");
+		int y2 = scan.nextInt();
+		
+		double length = Math.sqrt((Math.pow((x2-x1), 2))+(Math.pow((y2-y1), 2)));
+		
+		System.out.println("Length of The Line is : "+length);
+	}
+
+}
