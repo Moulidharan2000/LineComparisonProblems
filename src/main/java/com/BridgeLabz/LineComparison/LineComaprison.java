005606564e3230602f826d1fd517ffaab5648e77
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-01-04 14:43:07 +0530

    Added : Check Equality of Two Lines

--- src/main/java/com/BridgeLabz/LineComparison/LineComaprison.java
+++ src/main/java/com/BridgeLabz/LineComparison/LineComaprison.java
@@ -17,9 +17,25 @@ public class LineComaprison {
 		System.out.print("Enter the Number for y2 : ");
 		int y2 = scan.nextInt();
 		
-		double length = Math.sqrt((Math.pow((x2-x1), 2))+(Math.pow((y2-y1), 2)));
+		System.out.print("Enter the Number for P1 : ");
+		int p1 = scan.nextInt();
+		System.out.print("Enter the Number for Q1 : ");
+		int q1 = scan.nextInt();
+		System.out.print("Enter the Number for P2 : "); 
+		int p2 = scan.nextInt();
+		System.out.print("Enter the Number for Q2 : ");
+		int q2 = scan.nextInt();
 		
-		System.out.println("Length of The Line is : "+length);
+		double length1 = Math.sqrt((Math.pow((x2-x1), 2))+(Math.pow((y2-y1), 2)));
+		double length2 = Math.sqrt((Math.pow((p2-p1), 2))+(Math.pow((q2-q1), 2)));
+		
+		
+		Double Value1 = new Double(length1);
+		Double Value2 = new Double(length2);
+		
+		if(Value1.equals(Value2)) 
+			System.out.println("Two Lines are Equal ");
+		else
+			System.out.println("Two Lines are Not Equal ");
 	}
-
 }
